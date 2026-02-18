97ac24f06fb6af2b02cfb7f8de0b5f020e2ef6c6
rionainm <rionainm@example.com>
2026-02-18 23:06:15 +0700

    Fix: robust event handling for sidebar menu (off/on, stop animation, preventDefault)

--- public/assets/js/jquery.app.js
+++ public/assets/js/jquery.app.js
@@ -36,7 +36,7 @@
     Sidemenu.prototype.menuItemClick = function (e) {
       if (!$("#wrapper").hasClass("enlarged")) {
         if ($(this).parent().hasClass("has_sub")) {
-
+          e.preventDefault();
         }
         if (!$(this).hasClass("subdrop")) {
           // hide any open menus and remove all other classes
@@ -45,13 +45,13 @@
           $("#sidebar-menu .pull-right i").removeClass("md-remove").addClass("md-add");
 
           // open our new menu and add the open class
-          $(this).next("ul").slideDown(350);
+          $(this).next("ul").stop(true, true).slideDown(350);
           $(this).addClass("subdrop");
           $(".pull-right i", $(this).parents(".has_sub:last")).removeClass("md-add").addClass("md-remove");
           $(".pull-right i", $(this).siblings("ul")).removeClass("md-remove").addClass("md-add");
         } else if ($(this).hasClass("subdrop")) {
           $(this).removeClass("subdrop");
-          $(this).next("ul").slideUp(350);
+          $(this).next("ul").stop(true, true).slideUp(350);
           $(".pull-right i", $(this).parent()).removeClass("md-remove").addClass("md-add");
         }
       }
@@ -71,7 +71,7 @@
       });
 
       // LEFT SIDE MAIN NAVIGATION
-      $this.$menuItem.on(event, $this.menuItemClick);
+      $this.$menuItem.off(event).on(event, $this.menuItemClick);
 
       // NAVIGATION HIGHLIGHT & OPEN PARENT
       $("#sidebar-menu ul li.has_sub a.active").parents("li:last").children("a:first").addClass("active").trigger("click");
