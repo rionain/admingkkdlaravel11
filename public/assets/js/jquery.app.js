8f7fd6b015eae19d7b15b012b0bf118d2e4ccc05
rionainm <rionainm@example.com>
2026-02-24 10:43:56 +0700

    Fix: replace trigger('click') with direct show() to fix sidebar auto-collapse root cause

--- public/assets/js/jquery.app.js
+++ public/assets/js/jquery.app.js
@@ -74,7 +74,7 @@
       $this.$menuItem.off(event).on(event, $this.menuItemClick);
 
       // NAVIGATION HIGHLIGHT & OPEN PARENT
-      $("#sidebar-menu ul li.has_sub a.active").parents("li:last").children("a:first").addClass("active").trigger("click");
+      $("#sidebar-menu ul li.has_sub a.active").parents("li:last").children("a:first").addClass("active subdrop").next("ul").show();
     },
 
     //init Sidemenu
